ec0ee071b2813a7c9b5f8cdc7d961932f7df33f5
kingsleyesisi <kingsleyesisi@outlook.com>
2026-01-13 05:36:58 +0100

    Allow all origin

--- starwars_api/settings.py
+++ starwars_api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-m+kt1$!tg#8@7ka=9k^ng
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
